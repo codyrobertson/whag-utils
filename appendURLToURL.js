46827df9a773e6e81c713d2fd45d55474387b095
Cody Robertson <digitalcody@gmail.com>
2023-09-24 18:37:32 -0700

    Create appendURLToURL.js

---
+++ appendURLToURL.js
@@ -0,0 +1,14 @@
+<!-- Append Form Value To URL String -->
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById('wf-form-hero-url-input');
+    var input = document.getElementById('paste-url-input');
+    var submitButton = document.getElementById('verify-url-submit');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        var inputValue = input.value;
+        if (inputValue) {
+            window.location.href = "/join?url=" + encodeURIComponent(inputValue);
+        }
+    });
+});
