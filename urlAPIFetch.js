77ab33802343d59c0f66f75c928a0fc6be965f8a
Mackenzie Robertson <cody@polyient.io>
2023-09-27 12:12:18 -0700

    added new scripts

--- urlAPIFetch.js
+++ urlAPIFetch.js
@@ -1,39 +1,70 @@
 // Function to fetch data from the API and populate the data on the page
 function fetchDataAndPopulate() {
-    // Extract the URL from the query parameter
-    var urlParams = new URLSearchParams(window.location.search);
-    var inputURL = urlParams.get('pasteURLinput'); // Changed from 'url' to 'pasteURLinput'
+    try {
+        // Extract the URL from the query parameter
+        var urlParams = new URLSearchParams(window.location.search);
+        var inputURL = urlParams.get('pasteURLinput');
 
-    // Decode the URL
-    var decodedURL = decodeURIComponent(inputURL);
+        // If no URL, exit the function
+        if (!inputURL) throw 'No URL parameter found';
 
-    // Fetch data from the API
-    fetch(`https://jsonlink.io/api/extract?url=${decodedURL}`)
-        .then(response => response.json())
-        .then(data => {
-            // Populate the data on the page
-            populateData(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+        // Decode the URL
+        var decodedURL = decodeURIComponent(inputURL);
+
+        // Fetch data from the API
+        fetch(`https://jsonlink.io/api/extract?url=${decodedURL}`)
+            .then(response => response.json())
+            .then(data => {
+                // Populate the data on the page
+                populateData(data);
+                populateForm(data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    } catch (error) {
+        console.error(error);
+        populateForm({
+            url: null,
+            title: null,
+            description: null,
+            images: null
         });
+    }
 }
 
 // Function to populate the data
 function populateData(responseData) {
-    // Check if the required elements exist on the page
-    if (document.querySelector('[data-link-meta-url]') && document.querySelector('[data-mata-link-title]') && document.querySelector('[data-mata-link-description]') && document.querySelector('[data-meta-link-img]')) {
-        // Populate the data on the page
-        document.querySelector('[data-link-meta-url]').textContent = responseData.url || '';
-        document.querySelector('[data-mata-link-title]').textContent = responseData.title || '';
-        document.querySelector('[data-mata-link-description]').textContent = responseData.description || '';
-        if (responseData.images && responseData.images.length > 0) {
-            document.querySelector('[data-meta-link-img]').setAttribute('src', responseData.images[0]);
-        }
+    // Populate the data on the page
+    document.querySelector('[data-link-meta-url]').textContent = responseData.url || '';
+    document.querySelector('[data-mata-link-title]').textContent = responseData.title || '';
+    document.querySelector('[data-mata-link-description]').textContent = responseData.description || '';
+    if (responseData.images && responseData.images.length > 0) {
+        document.querySelector('[data-meta-link-img]').setAttribute('src', responseData.images[0]);
     }
 }
 
+// Function to populate the form with hidden inputs
+function populateForm(responseData) {
+    const form = document.querySelector('[data-sysflow-query-form="pasteURLinput"]');
+    if (form) {
+        appendHiddenInput(form, 'url', responseData.url);
+        appendHiddenInput(form, 'title', responseData.title);
+        appendHiddenInput(form, 'description', responseData.description);
+        appendHiddenInput(form, 'image', responseData.images ? responseData.images[0] : null);
+    }
+}
+
+// Function to append hidden input to form
+function appendHiddenInput(form, name, value) {
+    const input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = name;
+    input.value = value || '';
+    form.appendChild(input);
+}
+
 // Call the function to fetch the data and populate the data on page load
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetchDataAndPopulate();
 });
